28e89c1c8af57b66c447310d8e12f7ce54329cc0
azar-intelops <amohammed@appstekcorp.com>
2023-04-11 19:53:30 +0530

    code refactor

--- test/calculation.go
+++ test/calculation.go
@@ -5,9 +5,6 @@ import (
 )
 
 func Add(a int64, b int64) int64 {
-
-	// log.Fatalf("sum of c value is %t shold be ",arguments.a,arguments.b)
-
 	result := a + b
 	return result
 }
@@ -26,11 +23,11 @@ func Divid(c int64, d int64) int64 {
 	return res
 }
 
-func isJSONString(s string) bool {
+func IsJSONString(s string) bool {
 	var js string
 	return json.Unmarshal([]byte(s), &js) == nil
 }
-func isJSON(s string) bool {
+func IsJSON(s string) bool {
 	var js map[string]interface{}
 	return json.Unmarshal([]byte(s), &js) == nil
 }
