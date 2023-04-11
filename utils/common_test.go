2bf4436f265617f5f60681a4af835b3e508fa083
azar-intelops <amohammed@appstekcorp.com>
2023-04-11 19:11:45 +0530

    code coverage generated

--- utils/common_test.go
+++ utils/common_test.go
@@ -1,11 +1,37 @@
 package utils
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type IsEmpty_TestCases struct {
+	value    string
+	expected bool
+}
 
 func Test_IsEmpty(t *testing.T) {
-	exp := true
-	res := IsEmpty("")
-	if exp != res {
-		t.Fatalf("got %t, wanted %t", res, exp)
+	data := []IsEmpty_TestCases{
+		{
+			value:    "",
+			expected: true,
+		},
+		{
+			value:    "world",
+			expected: false,
+		},
+		{
+			value:    "hello",
+			expected: true,
+		},
+	}
+	for _, testcase := range data {
+		t.Run(fmt.Sprintf("IsEmpty(%v) == %t", testcase.value, testcase.expected), func(t *testing.T) {
+			res := IsEmpty(testcase.value)
+			if testcase.expected != res {
+				t.Fatalf("got %t, wanted %t", res, testcase.expected)
+			}
+		})
+
 	}
 }
